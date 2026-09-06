7df541b15ade907971b5b2b32f5f20d95f1e3de4
Ahmad Sarayrah <ahmadsarayrah1122@gmail.com>
2026-09-06 03:13:58 +0300

    Last Update

--- LibrarySystem.DataAccess/clsDataAccessLayerPeople.cs
+++ LibrarySystem.DataAccess/clsDataAccessLayerPeople.cs
@@ -1,186 +1,114 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Security.Policy;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibrarySystem.DataAccess
 {
     public class clsDataAccessLayerPeople
     {
- 
- 
-
-         enum enMode  {AddNew=1,Update=2};
-       public int PersonID {  get; private set; }
-       public string FirstName { get;  set; }
-
-        public string LastName { get; set; }
-        public string Phone {  get; set; }
-        public string Email { get; set; }
-
-
-        private enMode _Mode=enMode.AddNew;
-       public clsDataAccessLayerPeople()
-        {
-            this.PersonID = -1;
-            this.FirstName = string.Empty;
-            this.LastName = string.Empty;
-            this.Phone = string.Empty;
-            this.Email = string.Empty;
-            this._Mode = enMode.AddNew;
- 
-        }
-        public clsDataAccessLayerPeople(string FirstName,string LastName,string Phone,string Email)
-        {
-            this.FirstName = FirstName;
-            this.LastName = LastName;
-            this.Phone = Phone;
-            this.Email = Email;
-            this._Mode = enMode.Update;
-
-
-        }
-
- 
- 
-         
+        // 1. Read (Get By ID)
         public static bool GetPersonInfoByID(int personID, ref string firstName,
             ref string lastName, ref string phone, ref string email)
         {
             bool isFound = false;
-
-            string query = @"SELECT [FirstName]
-                                ,[LastName]
-                                ,[Phone]
-                                ,[Email]
-                            FROM [dbo].[People]
-                            WHERE PersonID = @PersonID;";
+            string query = @"SELECT FirstName, LastName, Phone, Email 
+                             FROM People 
+                             WHERE PersonID = @PersonID;";
 
             using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
             using (SqlCommand command = new SqlCommand(query, connection))
             {
-                    command.Parameters.AddWithValue("@PersonID", personID);
+                command.Parameters.AddWithValue("@PersonID", personID);
 
                 try
                 {
                     connection.Open();
-                     
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         if (reader.Read())
                         {
-                                isFound = true;
-
+                            isFound = true;
                             firstName = (string)reader["FirstName"];
                             lastName = (string)reader["LastName"];
-
-                                phone = (reader["Phone"] != DBNull.Value) ? (string)reader["Phone"] : "";
+                            phone = (reader["Phone"] != DBNull.Value) ? (string)reader["Phone"] : "";
                             email = (reader["Email"] != DBNull.Value) ? (string)reader["Email"] : "";
                         }
                     }
-
                 }
                 catch (Exception)
                 {
                     isFound = false;
                 }
             }
-
             return isFound;
         }
 
-        public static int AddNewPerson(string firstName ,string lastName,string phone,string email)
+        // 2. Create (AddNew)
+        public static int AddNewPerson(string firstName, string lastName, string phone, string email)
         {
             int personID = -1;
-
-            string Query = @"INSERT INTO [dbo].[People]
-                            ( FirstName 
-                           , LastName 
-                           , Phone 
-                           , Email )
-                            VALUES
-                           (@FirstName,@LastName,@Phone,@Email) 
-                           SELECT SCOPE_IDENTITY()  ";
+            string query = @"INSERT INTO People (FirstName, LastName, Phone, Email)
+                             VALUES (@FirstName, @LastName, @Phone, @Email);
+                             SELECT SCOPE_IDENTITY();";
 
             using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
-            using (SqlCommand command = new SqlCommand(Query, connection))
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
-
                 command.Parameters.AddWithValue("@FirstName", firstName);
-
                 command.Parameters.AddWithValue("@LastName", lastName);
-                
-            
 
-                if(string.IsNullOrEmpty( phone))
+                if (string.IsNullOrEmpty(phone))
                     command.Parameters.AddWithValue("@Phone", DBNull.Value);
                 else
                     command.Parameters.AddWithValue("@Phone", phone);
 
-                if(string.IsNullOrEmpty( email))
+                if (string.IsNullOrEmpty(email))
                     command.Parameters.AddWithValue("@Email", DBNull.Value);
                 else
                     command.Parameters.AddWithValue("@Email", email);
 
-
-
                 try
                 {
                     connection.Open();
-                    
-                    Object result = command.ExecuteScalar();
-
-                     
+                    object result = command.ExecuteScalar();
 
                     if (result != null && int.TryParse(result.ToString(), out int insertedID))
                     {
-                        personID = insertedID;  
+                        personID = insertedID;
                     }
-
                 }
-                catch
+                catch (Exception)
                 {
                     return -1;
                 }
-
-
             }
             return personID;
-
         }
 
-
-        public static bool UpdatePerson(int personID,string firstName,string lastName,string phone,string email)
+        // 3. Update
+        public static bool UpdatePerson(int personID, string firstName, string lastName, string phone, string email)
         {
-
-             int rowAffected = -1;
-
-            string Query = @"UPDATE [dbo].[People]
-                            SET [FirstName] =  @FirstName 
-                            ,[LastName] =  @LastName 
-                            ,[Phone] =  @Phone 
-                            ,[Email] =  @Email 
-                            WHERE PersonID =@PersonID";
-
+            int rowsAffected = 0;
+            string query = @"UPDATE People 
+                             SET FirstName = @FirstName,
+                                 LastName  = @LastName,
+                                 Phone     = @Phone,
+                                 Email     = @Email
+                             WHERE PersonID = @PersonID;";
 
             using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
-            using (SqlCommand command = new SqlCommand(Query, connection)) {
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
                 command.Parameters.AddWithValue("@PersonID", personID);
                 command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
 
-               command.Parameters.AddWithValue("@LastName", lastName);
-
-
-                if(string.IsNullOrEmpty(phone))
+                if (string.IsNullOrEmpty(phone))
                     command.Parameters.AddWithValue("@Phone", DBNull.Value);
                 else
                     command.Parameters.AddWithValue("@Phone", phone);
 
-                if(string.IsNullOrEmpty(email))
+                if (string.IsNullOrEmpty(email))
                     command.Parameters.AddWithValue("@Email", DBNull.Value);
                 else
                     command.Parameters.AddWithValue("@Email", email);
@@ -188,34 +116,23 @@ namespace LibrarySystem.DataAccess
                 try
                 {
                     connection.Open();
-
-                     rowAffected= command.ExecuteNonQuery();
-
-                    
-
+                    rowsAffected = command.ExecuteNonQuery();
                 }
-                catch
+                catch (Exception)
                 {
                     return false;
-
                 }
-            
-            
             }
-            return (rowAffected > 0);
-
+            return (rowsAffected > 0);
         }
 
-        public static bool DeletePerson(int personID )
+        // 4. Delete
+        public static bool DeletePerson(int personID)
         {
-            int rowAffected = -1;
+            int rowsAffected = 0;
+            string query = @"DELETE FROM People WHERE PersonID = @PersonID;";
 
-
-            string query = @"DELETE FROM [dbo].[People]
-                       WHERE PersonID= @PersonID";
-
-
-            using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString)) 
+            using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
             using (SqlCommand command = new SqlCommand(query, connection))
             {
                 command.Parameters.AddWithValue("@PersonID", personID);
@@ -223,30 +140,67 @@ namespace LibrarySystem.DataAccess
                 try
                 {
                     connection.Open();
-                    rowAffected = command.ExecuteNonQuery();
-
+                    rowsAffected = command.ExecuteNonQuery();
                 }
-                catch
+                catch (Exception)
                 {
-                    rowAffected = -1;
-
+                    return false;
                 }
-
-
             }
-            return (rowAffected > 0);
-     
+            return (rowsAffected > 0);
+        }
 
+        // 5. Read All (DataTable)
+        public static DataTable GetAllPeople()
+        {
+            DataTable dt = new DataTable();
+            string query = @"SELECT PersonID, FirstName, LastName, Phone, Email FROM People ORDER BY PersonID DESC;";
 
-        } 
+            using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            dt.Load(reader);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                }
+            }
+            return dt;
+        }
 
+        // 6. Check Existence
+        public static bool IsPersonExist(int personID)
+        {
+            bool isFound = false;
+            string query = @"SELECT 1 FROM People WHERE PersonID = @PersonID;";
 
+            using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@PersonID", personID);
+
+                try
+                {
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    isFound = (result != null);
+                }
+                catch (Exception)
+                {
+                    isFound = false;
+                }
+            }
+            return isFound;
+        }
     }
-
-
 }
-
-
-
-
- 
--- LibrarySystem.DataAccess/clsDataAccessLayerUsers.cs
+++ LibrarySystem.DataAccess/clsDataAccessLayerUsers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace LibrarySystem.DataAccess
@@ -6,11 +7,11 @@ namespace LibrarySystem.DataAccess
     public class clsDataAccessLayerUsers
     {
         public static bool Login(string username, string password,
-            ref int userID, ref int personID, ref bool isActive)
+            ref int userID, ref int personID, ref bool isActive , ref int permissions )
         {
             bool isFound = false;
             // query 
-            string query = @"SELECT UserID, PersonID, IsActive 
+            string query = @"SELECT UserID, PersonID, IsActive ,Permissions
                              FROM Users 
                              WHERE Username = @Username AND Password = @Password;";
             
@@ -34,7 +35,7 @@ namespace LibrarySystem.DataAccess
                             isFound = true;
                             userID = (int)reader["UserID"];
                             personID = (int)reader["PersonID"];
-                           
+                            permissions = (int)reader["[Permissions]"];
                             // deal with null case.
                             isActive = reader["IsActive"] != DBNull.Value && (bool)reader["IsActive"];
                         }
@@ -48,5 +49,211 @@ namespace LibrarySystem.DataAccess
 
             return isFound;
         }
+
+
+        public static bool GetUserByID(int userID,ref int personID,ref string username,ref  string password,ref bool isActive,ref int permissions) 
+        {
+
+            bool isFound = false;
+
+            string query = @"SELECT 
+                             [PersonID]
+                            ,[Username]
+                            ,[Password]
+                            ,[IsActive]
+                            ,[Permissions]
+                            FROM [dbo].[Users] WHERE UserID =@userID";
+
+
+            using(SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query , connection))
+            {
+
+
+                command.Parameters.AddWithValue("@userID",userID);
+
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        if (reader.Read())
+                        {
+                            personID = (int)reader["PersonID"];
+                            username = (string)reader["Username"];
+                            password = (string)reader["Password"];
+                            isActive = reader["IsActive"] != DBNull.Value && (bool)reader["IsActive"];
+                            permissions = (int)reader["Permissions"];
+                            isFound = true;
+                        }
+                    }
+                }
+                catch
+                {
+                    isFound = false;
+                }
+
+
+            }
+            return isFound;
+        
+        }
+        
+        public static bool GetUserByPersonID(  int personID,ref int userID,ref string username,ref  string password,ref bool isActive,ref int permissions) 
+        {
+
+            bool isFound = false;
+
+            string query = @"SELECT 
+                             [UserID]
+                            ,[Username]
+                            ,[Password]
+                            ,[IsActive]
+                            ,[Permissions]
+                            FROM [dbo].[Users] WHERE PersonID =@personID";
+
+
+            using(SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query , connection))
+            {
+
+
+                command.Parameters.AddWithValue("@personID", personID);
+
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        if (reader.Read())
+                        {
+                            userID = (int)reader["UserID"];
+                            username = (string)reader["Username"];
+                            password = (string)reader["Password"];
+                            isActive = reader["IsActive"] != DBNull.Value && (bool)reader["IsActive"];
+                            permissions = (int)reader["Permissions"];
+                            isFound = true;
+                        }
+                    }
+                    
+                }
+                catch
+                {
+                    isFound = false;
+                }
+
+
+            }
+            return isFound;
+        
+        }
+
+
+        public static int AddNewUser(int PersonID,string Username, string Password, bool IsActive,int Permissions )
+        {
+
+            int UserID = -1;
+
+            string query = @"INSERT INTO [dbo].[Users]
+                                           ([PersonID]
+                                           ,[Username]
+                                           ,[Password]
+                                           ,[IsActive]
+                                           ,[Permissions])
+                                     VALUES
+                                           (@PersonID
+                                           ,@Username
+                                           ,@Password
+                                           ,@IsActive
+                                           ,@Permissions);
+
+                                SELECT SCOPE_IDENTITY();";
+
+
+            using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection)) 
+            {
+                  command.Parameters.AddWithValue("@PersonID", PersonID);
+
+                 command.Parameters.AddWithValue("@Username", Username);
+
+                 command.Parameters.AddWithValue("@Password", Password);
+
+                 command.Parameters.AddWithValue("@IsActive", IsActive);
+
+                 command.Parameters.AddWithValue("@Permissions", Permissions);
+
+                try
+                {
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+
+
+                    if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                    {
+
+                        UserID =insertedID ;
+
+
+                    }
+
+                }
+                catch
+                {
+                    UserID = -1;
+
+                }
+
+            }
+            return UserID;
+
+        }
+
+        public static bool UpdateUser(int UserID, string Username, string Password, bool IsActive, int Permissions)
+        {
+
+            int rowAfficted = 0;
+
+            string query = @"UPDATE [dbo].[Users]
+                            SET  [Username] =  @Username 
+                                ,[Password] = @Password 
+                                ,[IsActive] =  @IsActive 
+                                ,[Permissions] =  @Permissions 
+                            WHERE UserID=@UserID";
+
+
+            using (SqlConnection connection = new SqlConnection(clsSettingsDataAccessLayer.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+
+
+                command.Parameters.AddWithValue("@UserID", UserID);
+                command.Parameters.AddWithValue("@Username", Username);
+                command.Parameters.AddWithValue(@"Password", Password);                       
+                command.Parameters.AddWithValue("@IsActive", IsActive); 
+                command.Parameters.AddWithValue("@Permissions", Permissions);
+
+                try
+                {
+                    connection.Open();
+
+                    rowAfficted = command.ExecuteNonQuery();
+
+
+                }
+                catch
+                {
+                    rowAfficted = -1;
+                }
+ 
+            
+            }
+
+            return rowAfficted > 0;
+        }
+
+
     }
 }
