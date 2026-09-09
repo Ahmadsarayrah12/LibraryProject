dbe1edc515fbb6ebc6ac042a04c8da00cf2a87d3
Ahmad Sarayrah <ahmadsarayrah1122@gmail.com>
2026-09-09 20:36:52 +0300

    add filters for Users

--- LibrarySystem.UI/frmUsers.Designer.cs
+++ LibrarySystem.UI/frmUsers.Designer.cs
@@ -28,13 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvUsers = new System.Windows.Forms.DataGridView();
             this.lblRecordsCount = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbFilterBy = new System.Windows.Forms.ComboBox();
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.cbIsActive = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,85 +44,118 @@
             // 
             this.dgvUsers.AllowUserToAddRows = false;
             this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.AllowUserToResizeRows = false;
             this.dgvUsers.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Tahoma", 8F);
-            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvUsers.DefaultCellStyle = dataGridViewCellStyle8;
-            this.dgvUsers.Location = new System.Drawing.Point(36, 223);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvUsers.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvUsers.Location = new System.Drawing.Point(36, 220);
+            this.dgvUsers.MultiSelect = false;
             this.dgvUsers.Name = "dgvUsers";
             this.dgvUsers.ReadOnly = true;
-            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Tahoma", 8F);
-            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvUsers.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvUsers.Size = new System.Drawing.Size(1028, 362);
             this.dgvUsers.TabIndex = 0;
-            this.dgvUsers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_CellContentClick);
             // 
             // lblRecordsCount
             // 
             this.lblRecordsCount.AutoSize = true;
-            this.lblRecordsCount.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsCount.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRecordsCount.Location = new System.Drawing.Point(32, 607);
             this.lblRecordsCount.Name = "lblRecordsCount";
-            this.lblRecordsCount.Size = new System.Drawing.Size(59, 23);
+            this.lblRecordsCount.Size = new System.Drawing.Size(176, 19);
             this.lblRecordsCount.TabIndex = 1;
-            this.lblRecordsCount.Text = "label1";
+            this.lblRecordsCount.Text = "Count Of Records: 0";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(463, 13);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(463, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 33);
+            this.label1.Size = new System.Drawing.Size(183, 33);
             this.label1.TabIndex = 2;
             this.label1.Text = "User Screen";
             // 
-            // comboBox1
+            // cbFilterBy
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "None ",
+            this.cbFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilterBy.FormattingEnabled = true;
+            this.cbFilterBy.Items.AddRange(new object[] {
+            "None",
             "User ID",
             "Person ID",
             "Full Name",
             "Username",
             "Is Active"});
-            this.comboBox1.Location = new System.Drawing.Point(59, 141);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 3;
+            this.cbFilterBy.Location = new System.Drawing.Point(36, 177);
+            this.cbFilterBy.Name = "cbFilterBy";
+            this.cbFilterBy.Size = new System.Drawing.Size(160, 21);
+            this.cbFilterBy.TabIndex = 3;
+            this.cbFilterBy.SelectedIndexChanged += new System.EventHandler(this.cbFilterBy_SelectedIndexChanged);
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.Location = new System.Drawing.Point(210, 177);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(220, 20);
+            this.txtFilterValue.TabIndex = 4;
+            this.txtFilterValue.Visible = false;
+            this.txtFilterValue.TextChanged += new System.EventHandler(this.txtFilterValue_TextChanged);
+            this.txtFilterValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilterValue_KeyPress);
+            // 
+            // cbIsActive
+            // 
+            this.cbIsActive.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbIsActive.FormattingEnabled = true;
+            this.cbIsActive.Items.AddRange(new object[] {
+            "All",
+            "Yes",
+            "No"});
+            this.cbIsActive.Location = new System.Drawing.Point(210, 177);
+            this.cbIsActive.Name = "cbIsActive";
+            this.cbIsActive.Size = new System.Drawing.Size(120, 21);
+            this.cbIsActive.TabIndex = 5;
+            this.cbIsActive.Visible = false;
+            this.cbIsActive.SelectedIndexChanged += new System.EventHandler(this.cbIsActive_SelectedIndexChanged);
             // 
             // frmUsers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1093, 648);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbIsActive);
+            this.Controls.Add(this.txtFilterValue);
+            this.Controls.Add(this.cbFilterBy);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblRecordsCount);
             this.Controls.Add(this.dgvUsers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmUsers";
-            this.Text = "frmUsers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manage Users";
             this.Load += new System.EventHandler(this.frmUsers_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
             this.ResumeLayout(false);
@@ -133,6 +168,8 @@
         private System.Windows.Forms.DataGridView dgvUsers;
         private System.Windows.Forms.Label lblRecordsCount;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbFilterBy;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.ComboBox cbIsActive;
     }
 }
--- LibrarySystem.UI/frmUsers.cs
+++ LibrarySystem.UI/frmUsers.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using LibrarySystem.Business;
 
@@ -20,17 +14,19 @@ namespace LibrarySystem.UI
 
         private DataTable _dtAllUsers;
 
-
-        
+        private void frmUsers_Load(object sender, EventArgs e)
+        {
+            _RefreshUsersList();
+            cbFilterBy.SelectedIndex = 0;
+        }
 
         private void _RefreshUsersList()
         {
             _dtAllUsers = clsUser.GetAllUsers();
             dgvUsers.DataSource = _dtAllUsers;
 
-            lblRecordsCount.Text =  "Count Of Records: "+  dgvUsers.Rows.Count.ToString()  ;
+            lblRecordsCount.Text = "Count Of Records: " + dgvUsers.Rows.Count.ToString();
 
-            // تنسيق الأعمدة واختيار العرض المناسب بعد تحميل البيانات
             if (dgvUsers.Rows.Count > 0)
             {
                 dgvUsers.Columns["UserID"].HeaderText = "User ID";
@@ -48,8 +44,8 @@ namespace LibrarySystem.UI
                 dgvUsers.Columns["Permissions"].HeaderText = "Permissions";
                 dgvUsers.Columns["Permissions"].Width = 100;
 
-                dgvUsers.Columns["isActive"].HeaderText = "Is Active";
-                dgvUsers.Columns["isActive"].Width = 90;
+                dgvUsers.Columns["IsActive"].HeaderText = "Is Active";
+                dgvUsers.Columns["IsActive"].Width = 90;
 
                 dgvUsers.Columns["Phone"].HeaderText = "Phone";
                 dgvUsers.Columns["Phone"].Width = 120;
@@ -59,14 +55,117 @@ namespace LibrarySystem.UI
             }
         }
 
-        private void frmUsers_Load(object sender, EventArgs e)
+        private void cbFilterBy_SelectedIndexChanged(object sender, EventArgs e)
         {
-            _RefreshUsersList();
+            if (cbFilterBy.Text == "Is Active")
+            {
+                txtFilterValue.Visible = false;
+                cbIsActive.Visible = true;
+                cbIsActive.Focus();
+                cbIsActive.SelectedIndex = 0;
+            }
+            else
+            {
+                txtFilterValue.Visible = (cbFilterBy.Text != "None");
+                cbIsActive.Visible = false;
+
+                if (cbFilterBy.Text == "None")
+                {
+                    _dtAllUsers.DefaultView.RowFilter = "";
+                    lblRecordsCount.Text = "Count Of Records: " + dgvUsers.Rows.Count.ToString();
+                }
+                else
+                {
+                    txtFilterValue.Text = "";
+                    txtFilterValue.Focus();
+                }
+            }
         }
 
-        private void dgvUsers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void txtFilterValue_TextChanged(object sender, EventArgs e)
         {
+            string filterColumn = "";
 
+            switch (cbFilterBy.Text)
+            {
+                case "User ID":
+                    filterColumn = "UserID";
+                    break;
+
+                case "Person ID":
+                    filterColumn = "PersonID";
+                    break;
+
+                case "Full Name":
+                    filterColumn = "FullName";
+                    break;
+
+                case "Username":
+                    filterColumn = "Username";
+                    break;
+
+                default:
+                    filterColumn = "None";
+                    break;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtFilterValue.Text) || filterColumn == "None")
+            {
+                _dtAllUsers.DefaultView.RowFilter = "";
+                lblRecordsCount.Text = "Count Of Records: " + dgvUsers.Rows.Count.ToString();
+                return;
+            }
+
+            if (filterColumn == "UserID" || filterColumn == "PersonID")
+            {
+                if (int.TryParse(txtFilterValue.Text.Trim(), out int value))
+                {
+                    _dtAllUsers.DefaultView.RowFilter = $"{filterColumn} = {value}";
+                }
+                else
+                {
+                    _dtAllUsers.DefaultView.RowFilter = $"{filterColumn} = -1";
+                }
+            }
+            else
+            {
+                _dtAllUsers.DefaultView.RowFilter = $"{filterColumn} LIKE '{txtFilterValue.Text.Trim()}%'";
+            }
+
+            lblRecordsCount.Text = "Count Of Records: " + dgvUsers.Rows.Count.ToString();
+        }
+
+        private void cbIsActive_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filterValue = cbIsActive.Text;
+
+            switch (filterValue)
+            {
+                case "All":
+                    _dtAllUsers.DefaultView.RowFilter = "";
+                    break;
+
+                case "Yes":
+                    _dtAllUsers.DefaultView.RowFilter = "IsActive = 1";
+                    break;
+
+                case "No":
+                    _dtAllUsers.DefaultView.RowFilter = "IsActive = 0";
+                    break;
+            }
+
+            lblRecordsCount.Text = "Count Of Records: " + dgvUsers.Rows.Count.ToString();
+        }
+
+        private void txtFilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (cbFilterBy.Text == "User ID" || cbFilterBy.Text == "Person ID")
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
